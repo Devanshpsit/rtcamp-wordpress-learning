303b165a5f5c16538eaa09d963ebaa5faa7db835
Devansh Dwivedi <143828827+Devanshpsit@users.noreply.github.com>
2026-01-10 23:08:39 +0530

    Add functions.php with basic theme setup

---
+++ wordpress-theme/my-first-theme/functions.php
@@ -0,0 +1,9 @@
+<?php
+// Theme setup function
+function my_first_theme_setup() {
+    add_theme_support('title-tag');
+}
+
+// Hooking the function to WordPress
+add_action('after_setup_theme', 'my_first_theme_setup');
+?>
